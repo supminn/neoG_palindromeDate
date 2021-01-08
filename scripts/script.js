aa4860d45f6ac1614b12f02d25299aaedf385d06
supminn <30731236+supminn@users.noreply.github.com>
2021-01-08 22:50:47 +0530

    feat: display result on page

--- scripts/script.js
+++ scripts/script.js
@@ -17,36 +17,38 @@ date.setAttribute("max", maxDate);
 date.setAttribute("value", maxDate);
 
 //Find the nearest Palindrome
-const findPalindrome = function(str1, str2, str3, dateFormat){
-
+const findPalindrome = function (dateString) {
+    let curDate = new Date(dateString);
+    console.log(curDate);
 }
 
 //Calculate if palindrome
-const isPalindrome = (str1, str2, str3, dateFormat) => {
+const isPalindrome = (str1, str2, str3, dateFormat, dateString) => {
     let dateStr = str1 + str2 + str3;
     let revStr = dateStr.split('').reverse().join('');
     if (dateStr === revStr) {
         output.innerText += (dateFormat + " is a palindrome");
         document.querySelector('.txt-congo').innerText = "Congratulations! It's a palindrome\n";
     } else {
-        let nextDate = findPalindrome(str1, str2, str3, dateFormat);
+        let nextDate = findPalindrome(dateString);
         output.innerText += (dateFormat + ` is not a palindrome\nThe next palindrome value after your birthday is: ${nextDate}\n`);
         if (!document.querySelector('.txt-congo').innerText) {
             document.querySelector('.txt-congo').innerText = "None of the following could create a palindrome";
         }
     }
 }
+//Calls the function in different date format
+function callPalindrome() {
+    let dateVal = (date.value).split('-');
+    isPalindrome(dateVal[1], dateVal[2], dateVal[0], 'MM/DD/YYYY', dateVal.join('-'));
+    isPalindrome(dateVal[2], dateVal[1], dateVal[0], 'DD/MM/YYYY', dateVal.join('-'));
+    isPalindrome(dateVal[2], dateVal[1], dateVal[0].substring(2), 'MM/DD/YY', dateVal.join('-'));
+    isPalindrome(dateVal[1], dateVal[2], dateVal[0].substring(2), 'DD/MM/YY', dateVal.join('-'));
+}
 
 //Button trigger to check
-btn.addEventListener('click', () => {
-    let dateVal = (date.value).split('-');
-    isPalindrome(dateVal[1], dateVal[2], dateVal[0], 'MM/DD/YYYY');
-    isPalindrome(dateVal[2], dateVal[1], dateVal[0], 'DD/MM/YYYY');
-    isPalindrome(dateVal[2], dateVal[1], dateVal[0].substring(2), 'MM/DD/YY');
-    isPalindrome(dateVal[1], dateVal[2], dateVal[0].substring(2), 'DD/MM/YY');
-})
+btn.addEventListener('click', callPalindrome);
 
 // let startDate = new Date("01/01/2021"); 
 //   let todayDate = new Date();
 //   let diffInDays = Math.ceil((todayDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24));
-  
