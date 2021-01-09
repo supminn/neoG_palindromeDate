9d7f66ed93fa83f1cac78fa2ebd8fe6059bb87fe
supminn <30731236+supminn@users.noreply.github.com>
2021-01-09 19:58:12 +0530

    refactor: added switch case

--- scripts/script.js
+++ scripts/script.js
@@ -16,34 +16,52 @@ let maxDate = today.getFullYear() + '-' + mm + '-' + dd;
 date.setAttribute("max", maxDate);
 date.setAttribute("value", maxDate);
 
-//Find the nearest Palindrome
-const findPalindrome = function (dateString) {
-    let curDate = new Date(dateString);
-    console.log(curDate);
+const findNextPalindrome = () => {
+    
 }
 
-//Calculate if palindrome
-const isPalindrome = (str1, str2, str3, dateFormat, dateString) => {
-    let dateStr = str1 + str2 + str3;
+//Check if palindrome
+const isPalindrome = function (dateList, formatStr) {
+    let dateStr = "";
+    switch (formatStr) {
+        case 'MM/DD/YYYY':
+            dateStr = dateList[1] + dateList[2] + dateList[0];
+            break;
+        case 'DD/MM/YYYY':
+            dateStr = dateList[2] + dateList[1] + dateList[0];
+            break;
+        case 'MM/DD/YY':
+            dateStr = dateList[1] + dateList[2] + dateList[0].substring(2);
+            break;
+        case 'DD/MM/YY':
+            dateStr = dateList[2] + dateList[1] + dateList[0].substring(2);
+            break;
+    }
     let revStr = dateStr.split('').reverse().join('');
     if (dateStr === revStr) {
-        output.innerText += (dateFormat + " is a palindrome");
+        output.innerText += (formatStr + " is a palindrome\n");
         document.querySelector('.txt-congo').innerText = "Congratulations! It's a palindrome\n";
     } else {
-        let nextDate = findPalindrome(dateString);
-        output.innerText += (dateFormat + ` is not a palindrome\nThe next palindrome value after your birthday is: ${nextDate}\n`);
+        let nextDate = findNextPalindrome(dateList);
+        output.innerText += (formatStr + ` is not a palindrome\nThe next palindrome value after your birthday is: ${nextDate}\n`);
         if (!document.querySelector('.txt-congo').innerText) {
             document.querySelector('.txt-congo').innerText = "None of the following could create a palindrome";
         }
     }
 }
+
 //Calls the function in different date format
 function callPalindrome() {
+    output.innerText = '';
+    if (!date.value) {
+        output.innerText = `Provide a valid date.`;
+    }
     let dateVal = (date.value).split('-');
-    isPalindrome(dateVal[1], dateVal[2], dateVal[0], 'MM/DD/YYYY', dateVal.join('-'));
-    isPalindrome(dateVal[2], dateVal[1], dateVal[0], 'DD/MM/YYYY', dateVal.join('-'));
-    isPalindrome(dateVal[2], dateVal[1], dateVal[0].substring(2), 'MM/DD/YY', dateVal.join('-'));
-    isPalindrome(dateVal[1], dateVal[2], dateVal[0].substring(2), 'DD/MM/YY', dateVal.join('-'));
+    //Check if palindrome in 4 formats
+    isPalindrome(dateVal, 'MM/DD/YYYY');
+    isPalindrome(dateVal, 'DD/MM/YYYY');
+    isPalindrome(dateVal, 'MM/DD/YY');
+    isPalindrome(dateVal, 'DD/MM/YY');
 }
 
 //Button trigger to check
